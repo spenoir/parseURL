2b8d8ae86eefe51ec718f1a57cde93d5ccebe518
Adam Spence <aspence1@gmail.com>
2011-10-05 21:21:59 +0100

    added all initial files

---
+++ README.md
@@ -0,0 +1,27 @@
+This is a simple function that I've written as a jQuery plugin. It simply takes a url as a string and returns its attributes, much the same way that window.location does but with a bit more detail. 
+You could extend the window.location object with this function but I don't like messing with prototyping native javascript methods.
+
+
+Example Usage as a jquery plugin:
+
+$('a.link').parseURL(window.location.href);
+
+Example output:
+{
+	categorySlug: "spenoir",
+	host: "www.github.com",
+	parts: ["spenoir", "parseurl"],
+	path: "spenoir/parseurl",
+	protocol: "http:",
+	url: "http://www.github.com/spenoir/parseurl"
+}
+
+Example Usage with no argument:
+$('a.link').parseURL();
+
+If you pass no argument then parseURL will try to take the previous elements href attribute.
+
+
+
+In the pipeline:
+Demos, jasmine tests, example of extending window.location.
---
+++ scripts/uncompressed/parseURL.js
@@ -0,0 +1,67 @@
+(function ($) {
+
+                 /* This function parses a given url. relative or absolute
+                 *  It works in a similar way to window.location and returns a url Object
+                 *  url: the url to parse eg. 
+                 *      /spenoir/parseurl,
+                 *      spenoir/parseurl, (untested) 
+                 *      http://www.github.com/spenoir/parseurl,
+                 *      www.github.com/spenoir/parseurl,
+                 */
+                parseURL : function(url, catId) {
+                    var urlArr, urlObj = {}, queryStringArr, hasQueryString=false;
+                    //if url has query string then save it
+                    if ( $.inArray('?', url) >= 0 ) {                        
+                        hasQueryString = true;
+                        queryStringArr = url.split('?');   
+                        url = queryStringArr.shift();             
+                        urlObj.queryString = queryStringArr.shift().split("&");
+                    }
+                                        
+                    // if theres a hash then split and save that too
+                    if (url.match(/\#/)) {
+                        var pathArr = url.split('#');
+                        url = pathArr.shift();
+                        urlObj.hash = pathArr.shift();      // returns '/some/hash'
+                    }
+                    
+                    urlArr = url.split('/');  
+                    if (urlArr[0].length === 0) { // remove leading slash
+                        urlArr.shift();
+                    }
+
+                    if ( $.inArray('http:', urlArr) >= 0 ) {
+                        urlObj.protocol = urlArr.shift();   // returns 'http:'
+                        urlArr.shift();                     // removes empty item
+                        urlObj.host = urlArr.shift();       // returns 'www.github.com'
+                        urlObj.path = urlArr.join('/');     // returns 'spenoir/parseurl'
+                        urlObj.parts = urlArr;              // returns [spenoir, parseurl]
+                    
+                    // if url string starts with www. and no prtocol                                    
+                    } else if ( url.match(/www/) ) {  
+                        urlObj.protocol = null;
+                        urlObj.host = urlArr.shift();
+                        urlObj.path = urlArr.join('/');     // should add a leading slash here really
+                        urlObj.parts = urlArr;                      
+                    
+                    // if no protocol or host      
+                    } else {
+                        urlObj.protocol = null;
+                        urlObj.host = null
+                        urlObj.path = urlArr.join('/');
+                        urlObj.parts = urlArr;                        
+                    }                    
+                    
+                    // loop through url parts and save the category slug to urlObj
+                    for (i=0;i<urlObj.parts.length;i++) {
+                        var part = urlObj.parts[i];
+                        if ( catId && $.inArray(catId, part) ) {
+                            urlObj.categorySlug = part;     // returns 'spenoir'
+                            break;
+                        }                        
+                    }
+                    urlObj.url = url;                       // returns 'http://www.github.com/spenoir/parseurl'
+                    return urlObj;
+                };
+
+})();
